a26ada88ddc8f10733b3d4c7ad71976fa2921d61
Kenny <archer.2312@gmail.com>
2025-06-18 22:27:23 -0500

    # Carga de textura de la tierra y mejoras en la lógica.

--- CG-SolarSystem-Final/main.cpp
+++ CG-SolarSystem-Final/main.cpp
@@ -15,10 +15,9 @@ void createSphere(std::vector<float>& vertices, std::vector<unsigned int>& indic
     const int stackCount = 18;
     float radius = 1.0f;
 
-    float x, y, z, xy;                             
-    float nx, ny, nz, lengthInv = 1.0f / radius;    
-    float s, t;                                    
-
+    float x, y, z, xy;
+    float nx, ny, nz, lengthInv = 1.0f / radius;
+    float s, t;
     float sectorStep = 2 * PI / sectorCount;
     float stackStep = PI / stackCount;
     float sectorAngle, stackAngle;
@@ -27,47 +26,23 @@ void createSphere(std::vector<float>& vertices, std::vector<unsigned int>& indic
         stackAngle = PI / 2 - i * stackStep;
         xy = radius * cosf(stackAngle);
         z = radius * sinf(stackAngle);
-
         for (int j = 0; j <= sectorCount; ++j) {
             sectorAngle = j * sectorStep;
-
             x = xy * cosf(sectorAngle);
             y = xy * sinf(sectorAngle);
-            vertices.push_back(x);
-            vertices.push_back(y);
-            vertices.push_back(z);
-
-            nx = x * lengthInv;
-            ny = y * lengthInv;
-            nz = z * lengthInv;
-            vertices.push_back(nx);
-            vertices.push_back(ny);
-            vertices.push_back(nz);
-
-            s = (float)j / sectorCount;
-            t = (float)i / stackCount;
-            vertices.push_back(s);
-            vertices.push_back(t);
+            vertices.push_back(x); vertices.push_back(y); vertices.push_back(z);
+            nx = x * lengthInv; ny = y * lengthInv; nz = z * lengthInv;
+            vertices.push_back(nx); vertices.push_back(ny); vertices.push_back(nz);
+            s = (float)j / sectorCount; t = (float)i / stackCount;
+            vertices.push_back(s); vertices.push_back(t);
         }
     }
-
     int k1, k2;
     for (int i = 0; i < stackCount; ++i) {
-        k1 = i * (sectorCount + 1);
-        k2 = k1 + sectorCount + 1;
-
+        k1 = i * (sectorCount + 1); k2 = k1 + sectorCount + 1;
         for (int j = 0; j < sectorCount; ++j, ++k1, ++k2) {
-            if (i != 0) {
-                indices.push_back(k1);
-                indices.push_back(k2);
-                indices.push_back(k1 + 1);
-            }
-
-            if (i != (stackCount - 1)) {
-                indices.push_back(k1 + 1);
-                indices.push_back(k2);
-                indices.push_back(k2 + 1);
-            }
+            if (i != 0) { indices.push_back(k1); indices.push_back(k2); indices.push_back(k1 + 1); }
+            if (i != (stackCount - 1)) { indices.push_back(k1 + 1); indices.push_back(k2); indices.push_back(k2 + 1); }
         }
     }
 }
@@ -75,47 +50,37 @@ void createSphere(std::vector<float>& vertices, std::vector<unsigned int>& indic
 GLuint loadTexture(const char* path) {
     GLuint textureID;
     glGenTextures(1, &textureID);
-
     int width, height, nrComponents;
     unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
     if (data) {
         GLenum format;
-        if (nrComponents == 1)
-            format = GL_RED;
-        else if (nrComponents == 3)
-            format = GL_RGB;
-        else if (nrComponents == 4)
-            format = GL_RGBA;
-
+        if (nrComponents == 1) format = GL_RED;
+        else if (nrComponents == 3) format = GL_RGB;
+        else if (nrComponents == 4) format = GL_RGBA;
         glBindTexture(GL_TEXTURE_2D, textureID);
         glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
-
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        stbi_image_free(data);
         std::cout << "Textura cargada con exito: " << path << std::endl;
     }
     else {
         std::cout << "Error al cargar la textura: " << path << std::endl;
-        stbi_image_free(data);
+        std::cout << "Motivo del error (stb_image): " << stbi_failure_reason() << std::endl;
     }
-
-    return textureID;
+    stbi_image_free(data);
+    return data ? textureID : 0;
 }
 
 int main() {
-
     glfwInit();
-
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Mi Sistema Solar - Paso 1", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Sistema Solar v1", NULL, NULL);
     if (window == NULL) {
         std::cout << "Fallo al crear la ventana de GLFW" << std::endl;
         glfwTerminate();
@@ -129,86 +94,82 @@ int main() {
     }
     glEnable(GL_DEPTH_TEST);
 
-    float backgroundVertices[] = {
-        -1.0f, -1.0f, 0.0f,  0.0f, 0.0f,
-         1.0f, -1.0f, 0.0f,  1.0f, 0.0f,
-         1.0f,  1.0f, 0.0f,  1.0f, 1.0f,
-        -1.0f,  1.0f, 0.0f,  0.0f, 1.0f
-    };
-    unsigned int backgroundIndices[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
+    Shader ourShader("shaders/shader.vert", "shaders/shader.frag");
 
     std::vector<float> sphereVertices;
     std::vector<unsigned int> sphereIndices;
     createSphere(sphereVertices, sphereIndices);
 
-    unsigned int backgroundVAO, backgroundVBO, backgroundEBO;
-    glGenVertexArrays(1, &backgroundVAO);
-    glGenBuffers(1, &backgroundVBO);
-    glGenBuffers(1, &backgroundEBO);
-
-    glBindVertexArray(backgroundVAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, backgroundVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(backgroundVertices), backgroundVertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, backgroundEBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(backgroundIndices), backgroundIndices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
     unsigned int sphereVAO, sphereVBO, sphereEBO;
     glGenVertexArrays(1, &sphereVAO);
     glGenBuffers(1, &sphereVBO);
     glGenBuffers(1, &sphereEBO);
-
     glBindVertexArray(sphereVAO);
-
     glBindBuffer(GL_ARRAY_BUFFER, sphereVBO);
     glBufferData(GL_ARRAY_BUFFER, sphereVertices.size() * sizeof(float), &sphereVertices[0], GL_STATIC_DRAW);
-
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, sphereEBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sphereIndices.size() * sizeof(unsigned int), &sphereIndices[0], GL_STATIC_DRAW);
-
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
+    glEnableVertexAttribArray(2); 
 
     GLuint galaxyTexture = loadTexture("textures/galaxy.jpg");
     GLuint sunTexture = loadTexture("textures/sun.jpg");
+    GLuint earthTexture = loadTexture("textures/earth.jpg");
 
     while (!glfwWindowShouldClose(window)) {
-
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        glDepthMask(GL_FALSE);
-        glBindVertexArray(backgroundVAO);
-        glBindTexture(GL_TEXTURE_2D, galaxyTexture);
+        ourShader.use();
 
-        glDepthMask(GL_TRUE);
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+
+        if (display_h == 0) display_h = 1;
+
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)display_w / (float)display_h, 0.1f, 100.0f);
+        glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 3.0f, 10.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        ourShader.setMat4("projection", projection);
+        ourShader.setMat4("view", view);
+
+        glDepthMask(GL_FALSE);
+        glm::mat4 model_background = glm::mat4(1.0f);
+        model_background = glm::scale(model_background, glm::vec3(50.0f, 50.0f, 50.0f));
+        ourShader.setMat4("model", model_background);
+        glBindTexture(GL_TEXTURE_2D, galaxyTexture);
         glBindVertexArray(sphereVAO);
+        glDrawElements(GL_TRIANGLES, sphereIndices.size(), GL_UNSIGNED_INT, 0);
+        glDepthMask(GL_TRUE);
+
+        glm::mat4 model_sun = glm::mat4(1.0f);
+        model_sun = glm::scale(model_sun, glm::vec3(1.0f, 1.0f, 1.0f));
+        ourShader.setMat4("model", model_sun);
         glBindTexture(GL_TEXTURE_2D, sunTexture);
+        glDrawElements(GL_TRIANGLES, sphereIndices.size(), GL_UNSIGNED_INT, 0);
+
+        glm::mat4 model_earth = glm::mat4(1.0f);
+        model_earth = glm::translate(model_earth, glm::vec3(3.5f, 0.0f, 0.0f));
+
+        model_earth = glm::scale(model_earth, glm::vec3(0.3f, 0.3f, 0.3f));
+
+        ourShader.setMat4("model", model_earth);
+
+        glBindTexture(GL_TEXTURE_2D, earthTexture);
+
+        glDrawElements(GL_TRIANGLES, sphereIndices.size(), GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glDeleteVertexArrays(1, &backgroundVAO);
-    glDeleteBuffers(1, &backgroundVBO);
-    glDeleteBuffers(1, &backgroundEBO);
     glDeleteVertexArrays(1, &sphereVAO);
     glDeleteBuffers(1, &sphereVBO);
     glDeleteBuffers(1, &sphereEBO);
+    glDeleteProgram(ourShader.ID);
 
     glfwTerminate();
     return 0;
